94b13c6b7f359d14669416096d40054c48b3935b
[marcos ewbank] <marcos.suarez@pivotree.com>
2022-02-14 09:41:23 -0300

    space

---
+++ main.js
@@ -0,0 +1,69 @@
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const texture = new THREE.TextureLoader().load("moon.png");
+
+const geometry = new THREE.SphereGeometry(5, 15, 16);
+const material = new THREE.MeshBasicMaterial({ map: texture });
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.001;
+  torus.rotation.y += 0.001;
+  torus.rotation.z += 0.001;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
